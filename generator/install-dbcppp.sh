94727743d894c5588e403cfef927a59bd667d237
Ben Hall <rcmast3r1@gmail.com>
2023-12-25 12:45:32 -0500

    removed old files and added ldconfig stage for image

--- generator/install-dbcppp.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# git clone --recurse-submodules https://github.com/xR3b0rn/dbcppp.git
-cd dbcppp
-# mkdir build
-cd build
-
-cmake -DCMAKE_BUILD_TYPE=Release ..
-make -j
-make RunTests
